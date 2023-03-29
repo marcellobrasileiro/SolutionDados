9cfec01f49d44a2eef800462f5734d379776ff9f
Marcello Brasileiro <victor.brasileiro6@hotmail.com>
2023-03-28 20:29:54 -0400

    Adicionar arquivos de projeto.

---
+++ GerarDado/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+namespace GerarDado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCriarArquivo = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.dgvFuncionarios = new System.Windows.Forms.DataGridView();
+            this.btnCriarLinhas = new System.Windows.Forms.Button();
+            this.sfdGravarArquivo = new System.Windows.Forms.SaveFileDialog();
+            this.txtNumeroFuncionarios = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCriarArquivo
+            // 
+            this.btnCriarArquivo.Enabled = false;
+            this.btnCriarArquivo.Location = new System.Drawing.Point(12, 456);
+            this.btnCriarArquivo.Name = "btnCriarArquivo";
+            this.btnCriarArquivo.Size = new System.Drawing.Size(335, 35);
+            this.btnCriarArquivo.TabIndex = 0;
+            this.btnCriarArquivo.Text = "Criar arquivo";
+            this.btnCriarArquivo.UseVisualStyleBackColor = true;
+            this.btnCriarArquivo.Click += new System.EventHandler(this.btnCriarArquivo_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Enabled = false;
+            this.btnReiniciar.Location = new System.Drawing.Point(363, 456);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(203, 35);
+            this.btnReiniciar.TabIndex = 1;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // dgvFuncionarios
+            // 
+            this.dgvFuncionarios.AllowUserToAddRows = false;
+            this.dgvFuncionarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFuncionarios.Location = new System.Drawing.Point(12, 54);
+            this.dgvFuncionarios.Name = "dgvFuncionarios";
+            this.dgvFuncionarios.RowHeadersWidth = 51;
+            this.dgvFuncionarios.RowTemplate.Height = 24;
+            this.dgvFuncionarios.Size = new System.Drawing.Size(554, 387);
+            this.dgvFuncionarios.TabIndex = 2;
+            // 
+            // btnCriarLinhas
+            // 
+            this.btnCriarLinhas.Location = new System.Drawing.Point(264, 12);
+            this.btnCriarLinhas.Name = "btnCriarLinhas";
+            this.btnCriarLinhas.Size = new System.Drawing.Size(302, 30);
+            this.btnCriarLinhas.TabIndex = 3;
+            this.btnCriarLinhas.Text = "Criar linhas para registro";
+            this.btnCriarLinhas.UseVisualStyleBackColor = true;
+            this.btnCriarLinhas.Click += new System.EventHandler(this.btnCriarLinhas_Click);
+            // 
+            // sfdGravarArquivo
+            // 
+            this.sfdGravarArquivo.DefaultExt = "*.txt";
+            this.sfdGravarArquivo.Filter = "Arquivos textos|*.txt";
+            this.sfdGravarArquivo.Title = "Dados para geração de arquivo";
+            // 
+            // txtNumeroFuncionarios
+            // 
+            this.txtNumeroFuncionarios.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNumeroFuncionarios.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNumeroFuncionarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumeroFuncionarios.Location = new System.Drawing.Point(145, 16);
+            this.txtNumeroFuncionarios.Name = "txtNumeroFuncionarios";
+            this.txtNumeroFuncionarios.Size = new System.Drawing.Size(100, 19);
+            this.txtNumeroFuncionarios.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(12, 20);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(127, 15);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "Nr. de Funcionarios:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(578, 503);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtNumeroFuncionarios);
+            this.Controls.Add(this.btnCriarLinhas);
+            this.Controls.Add(this.dgvFuncionarios);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btnCriarArquivo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCriarArquivo;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.DataGridView dgvFuncionarios;
+        private System.Windows.Forms.Button btnCriarLinhas;
+        private System.Windows.Forms.SaveFileDialog sfdGravarArquivo;
+        private System.Windows.Forms.TextBox txtNumeroFuncionarios;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
